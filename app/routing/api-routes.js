e93c894741adef6afd35bd8805aa8d489f5d471f
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-11-27 20:31:55 -0600

    updated app.post  data in api-routes.js

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -6,10 +6,18 @@ module.exports = function (app) {
     app.get("/api/tables" , function(req, res) {
         res.json(tableData);
     });
+
     app.get("/api/waitlist" , function(req, res) {
         res.json(waitListData);
     });
+
     app.post("/api/tables" , function(req, res) {
-        res.json(tableData);
+        if(tableData.length < 5) {
+            tableData.push(req.body);
+            res.json(true);
+        } else {
+            waitListData.push(req.body);
+            res.json(false);
+        }
     });
 }
