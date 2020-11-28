d1523056e35cb28ab8a9ee34879551e163d991a3
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-11-27 18:47:33 -0600

    added route in html-routes.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var bodyParser = require("body-parser");
 var express = require("express");
-var app = express();
 var path = require("path");
 
+var app = express();
 var PORT = process.env.PORT || 3001;
+
+
 
 app.use(bodyParser.urlencoded({extended: false}))
 
