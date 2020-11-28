2be38d6c43f50d80afb105623b1d350311221358
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-11-27 20:36:58 -0600

    updated reserve.html file location

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
     app.get("/tables", function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/tables.html"));
       });
-    app.get("/reservation", function(req, res) {
-        res.sendFile(path.join(__dirname + "/../public/reservation.html"));
+    app.get("/reserve", function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/reserve.html"));
       });
     app.use( function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/main.html"));
